ab1fab367d5c4fccda96345734be7f29522d0cbf
unknown <iizzeddin62@gmail.com>
2021-12-30 21:28:48 +0200

    return 401 on jwt malformed

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,9 +1,9 @@
-const {config} = require('dotenv');
+const { config } = require('dotenv');
 
 config();
 module.exports = {
   development: {
-    use_env_variable: 'DB_DEV',
+    use_env_variable: 'DATABASE_URL_DEV',
     dialect: 'postgres',
   },
   test: {
@@ -11,7 +11,13 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    use_env_variable: 'DB_PROD',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
